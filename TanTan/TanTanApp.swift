56c587b880629c878ae631cdc06276eb1c121f6f
hmf <934115331@qq.com>
2024-07-07 19:55:17 +0800

    Initial Commit

---
+++ TanTan/TanTanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanTanApp.swift
+//  TanTan
+//
+//  Created by hmf on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct TanTanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
